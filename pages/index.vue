59a76d7984ab54c95543fb2aab71ebbe7fe6bfaf
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-08-29 13:57:11 +0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center" style="margin-top: 100px">
+        <h1>Home page</h1>
+      </div>
+
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+
+  }
+}
+</script>
